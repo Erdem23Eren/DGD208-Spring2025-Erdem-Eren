0b80616baf26bce048660ba64c05f8032cba28ed
Erdem23Eren <153505986+Erdem23Eren@users.noreply.github.com>
2025-05-15 23:26:42 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿import time
+import threading
+import os
+
+class Animal:
+def __init__(self, name , animal_type)
+
+self = define.self
+name = define.animal_name
+type = define.animal_type
+
